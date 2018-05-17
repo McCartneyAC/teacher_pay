2de4929d5bd87848c3da7fb3d0b6bb97d2064781
McCartneyAC <acm9q@virginia.edu>
2018-05-17 00:56:01 -0400

    Add files via upload

--- plots.R
+++ plots.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ggthemes)
 library(readxl)
+library(magrittr)
 
 states<-read_xlsx("data.xlsx")
 states
@@ -19,4 +20,25 @@ Strike in 2018",
   theme_gdocs()+
   scale_color_fivethirtyeight()+
   scale_x_continuous(labels = scales::dollar)+
-  scale_y_continuous(labels = scales::dollar)
+  scale_y_continuous(labels = scales::dollar) + 
+  geom_abline(intercept = 0, slope =1)
+##########
+
+states %<>% 
+  mutate(pct_trump = `Trump Votes`/(`Clinton Votes` + `Trump Votes`)) %>% 
+  mutate(ratio_pay = `Adjusted Pay`/(`Actual Pay`))
+states %>% 
+  filter(Abbreviation !="DC") %>% 
+  ggplot(aes(y=ratio_pay, x = pct_trump)) + 
+  geom_text(aes(label=Abbreviation, color=factor(Strike))) +
+  geom_smooth(method = "lm", color="orange") +
+  scale_y_continuous(labels = scales::dollar) +
+  labs(
+    title="Teacher Pay and Politics by State",
+    y = "Ratio - Adjusted Pay over Actual Pay",
+    x = "Percent voted for Trump",
+    color = "Had Teacher 
+    Strike in 2018"
+  ) + 
+  theme_gdocs()+
+  scale_color_fivethirtyeight()
